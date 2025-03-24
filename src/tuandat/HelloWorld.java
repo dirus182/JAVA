aa72edf65d9913bab1ebb465bfb03a36cdcd435c
buituandat1822004@gmail <146466664+dirus182@users.noreply.github.com>
2025-03-24 20:37:34 +0700

    init project

---
+++ src/tuandat/HelloWorld.java
@@ -0,0 +1,10 @@
+package tuandat;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and Tuấn Đạt");
+		System.out.println("Hello world with java and Tuấn Đạt");
+		System.out.println("Hello world with java and Tuấn Đạt");
+		System.out.println("Hello world with java and Tuấn Đạt");
+	}
+}
